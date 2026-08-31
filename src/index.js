bdd96c49ce4b7b59281adc183aa4a8aa4b8eb45e
Sunny <77013204+sunny-bhakta@users.noreply.github.com>
2026-08-31 15:21:18 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const PORT = process.env.PORT || 3000;
-
+console.log("Hello....")
 function requestHandler(req, res) {
   if (req.url === '/health') {
     res.writeHead(200, { 'Content-Type': 'application/json' });
